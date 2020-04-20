526fb310278bcfbb415ccc4465a42a4bd7e0e3cf
qc542 <36024574+qc542@users.noreply.github.com>
2020-04-20 01:14:14 -0400

    Added more output to load_input_test Now the function also prints the raw forms of returned data

--- Futoshiki_tests.py
+++ Futoshiki_tests.py
@@ -1,3 +1,34 @@
 from Futoshiki import *
 
+def load_input_test() -> int:
+    [initial_state, constr] = load_input("input1.txt")
+    
+    print("load_input returns two lists: initial_state and constr.\n")
+    print("The following is load_input printed in its raw form: \n\n")
+    print(initial_state)
+    print('\n')
 
+    print("The following is constr printed in its raw form: \n\n")
+    print(constr)
+    print('\n')
+
+    for a_row in initial_state:
+        print(a_row)
+        print('\n')
+
+    for i in range(len(constr)):
+        for j in range(len(constr[i])):
+            print("Cell coordinates: Row " + str(i) + ", Column " +
+                    str(j) + '\n')
+            print("Cell constraints: \n")
+            
+            for k in range(len(constr[i][j])):
+                print(constr[i][j][k])
+                if (k != (len(constr[i][j]) - 1)):
+                    print(' ')
+
+            print('\n')
+
+    return 0
+
+load_input_test()
