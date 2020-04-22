7142ccd77f396e2a9aca3b39b37778b5419c35b0
qc542 <36024574+qc542@users.noreply.github.com>
2020-04-21 22:20:01 -0400

    Implemented initialize_board_test

--- Futoshiki_tests.py
+++ Futoshiki_tests.py
@@ -32,4 +32,25 @@ def load_input_test() -> int:
     return 0
 
 
+def initialize_board_test() -> int:
+
+    [initial_state, constr] = load_input("input1.txt")
+    a_board = initialize_board(initial_state, constr)
+
+    for i in range(0,5):
+        print("Row " + str(i) + ": \n")
+        for j in range(0,5):
+            a_cell = a_board.all_cells[i][j]
+            print("Cell coordinates: " + str(a_cell.coord) + '\n')
+            print("Assignment: " + str(a_cell.assign) + '\n')
+            print("Domain: " + str(a_cell.domain) + '\n')
+            print("Constraints: " + str(a_cell.constr) + '\n')
+            print('\n')
+
+        print('\n')
+
+    return 0
+
+
 load_input_test()
+initialize_board_test()
